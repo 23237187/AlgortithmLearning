d1e7fc4f71e27ac0d9bdcf49da1ae6e8987dd609
23237187 <uestc2803305011@gmail.com>
2015-06-02 22:19:00 +0800

    Add ch03 BST

--- src/yang/al/ch03/BST.java
+++ src/yang/al/ch03/BST.java
@@ -3,75 +3,77 @@ package yang.al.ch03;
 
 import yang.al.ch01.Queue;
 
+import java.util.Iterator;
+
 public class BST<Key extends Comparable<Key>, Value> {
     private Node root;
 
-    private class Node{
+    private class Node {
         private Key key;
         private Value val;
         private Node left, right;
         private int N;
 
-        public Node(Key key, Value val, int N){
+        public Node(Key key, Value val, int N) {
             this.key = key;
             this.val = val;
             this.N = N;
         }
     }
 
-    public int size(){
+    public int size() {
         return size(root);
     }
 
-    private int size(Node x){
-        if (x == null){
+    private int size(Node x) {
+        if (x == null) {
             return 0;
         } else {
             return x.N;
         }
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size() == 0;
     }
 
-    public Key min(){
+    public Key min() {
         if (isEmpty()) return null;
         return min(root).key;
     }
 
-    private Node min(Node x){
-        if (x.left == null){
+    private Node min(Node x) {
+        if (x.left == null) {
             return x;
         } else {
             return min(x.left);
         }
     }
 
-    public Key max(){
+    public Key max() {
         if (isEmpty()) return null;
         return max(root).key;
     }
 
-    private Node max(Node x){
-        if (x.right == null){
+    private Node max(Node x) {
+        if (x.right == null) {
             return x;
         } else {
             return max(x.right);
         }
     }
 
-    public Key floor(Key key){
+    public Key floor(Key key) {
         Node x = floor(root, key);
         if (x == null) return null;
         return x.key;
     }
 
-    private Node floor(Node x, Key key){
+    private Node floor(Node x, Key key) {
         if (x == null) return null;
         int cmp = key.compareTo(x.key);
         if (cmp == 0) return x;
-        if (cmp < 0){
+        if (cmp < 0) {
             return floor(x.left, key);
         }
 //      if (cmp > 0)
@@ -80,17 +82,17 @@ public class BST<Key extends Comparable<Key>, Value> {
         else return x;
     }
 
-    public Key ceiling(Key key){
+    public Key ceiling(Key key) {
         Node x = ceiling(root, key);
         if (x == null) return null;
         return x.key;
     }
 
-    private Node ceiling(Node x, Key key){
+    private Node ceiling(Node x, Key key) {
         if (x == null) return null;
         int cmp = key.compareTo(x.key);
         if (cmp == 0) return x;
-        if (cmp > 0){
+        if (cmp > 0) {
             return ceiling(x.right, key);
         }
         Node t = ceiling(x.left, key);
@@ -98,67 +100,67 @@ public class BST<Key extends Comparable<Key>, Value> {
         else return x;
     }
 
-    public Key select(int k){
+    public Key select(int k) {
         if (k < 0 || k >= size()) return null;
         return select(root, k).key;
     }
 
-    private Node select(Node x, int k){
+    private Node select(Node x, int k) {
         if (x == null) return null;
         int t = size(x.left);
-        if (t > k){
+        if (t > k) {
             return select(x.left, k);
-        } else if (t < k){
-            return select(x.right, k-t-1);
+        } else if (t < k) {
+            return select(x.right, k - t - 1);
         } else {
             return x;
         }
     }
 
-    public int rank(Key key){
+    public int rank(Key key) {
         return rank(root, key);
     }
 
-    private int rank(Node x, Key key){
+    private int rank(Node x, Key key) {
         if (x == null) return 0;
         int cmp = key.compareTo(x.key);
-        if (cmp < 0){
+        if (cmp < 0) {
             return rank(x.left, key);
-        } else if (cmp > 0){
+        } else if (cmp > 0) {
             return rank(x.right, key) + size(x.left) + 1;
         } else {
             return size(x.left);
         }
     }
 
-    public Value get(Key key){
+    public Value get(Key key) {
         return get(root, key);
     }
 
-    private Value get(Node x, Key key){
+    private Value get(Node x, Key key) {
         if (x == null) return null;
         int cmp = key.compareTo(x.key);
         if (cmp < 0) {
             return get(x.left, key);
-        } else if (cmp > 0){
+        } else if (cmp > 0) {
             return get(x.right, key);
         } else {
             return x.val;
         }
     }
 
-    public void put(Key key, Value val){
+    public void put(Key key, Value val) {
         root = put(root, key, val);
     }
 
-    private Node put(Node x, Key key, Value val){
+    private Node put(Node x, Key key, Value val) {
         if (x == null) {
             return new Node(key, val, 1);
         }
         int cmp = key.compareTo(x.key);
-        if (cmp < 0){
+        if (cmp < 0) {
             x.left = put(x.left, key, val);
-        } else if (cmp > 0){
+        } else if (cmp > 0) {
             x.right = put(x.right, key, val);
         } else {
             x.val = val;
@@ -167,40 +169,40 @@ public class BST<Key extends Comparable<Key>, Value> {
         return x;
     }
 
-    public boolean contains(Key key){
+    public boolean contains(Key key) {
         return get(key) != null;
     }
 
-    public Iterable<Key> keys(){
+    public Iterable<Key> keys() {
         return keys(min(), max());
     }
 
-    public Iterable<Key> keys(Key lo, Key hi){
+    public Iterable<Key> keys(Key lo, Key hi) {
         Queue<Key> queue = new Queue<Key>();
         keys(root, queue, lo, hi);
         return queue;
     }
 
-    private void keys(Node x, Queue<Key> queue, Key lo, Key hi){
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
         if (x == null) return;
         int cmplo = lo.compareTo(x.key);
         int cmphi = hi.compareTo(x.key);
-        if (cmplo < 0){
+        if (cmplo < 0) {
             keys(x.left, queue, lo, hi);
         }
-        if (cmplo <= 0 && cmphi >= 0){
+        if (cmplo <= 0 && cmphi >= 0) {
             queue.enqueue(x.key);
         }
-        if (cmphi > 0){
+        if (cmphi > 0) {
             keys(x.right, queue, lo, hi);
         }
     }
 
-    public void deleteMin(){
+    public void deleteMin() {
         root = deleteMin(root);
     }
 
-    private Node deleteMin(Node x){
+    private Node deleteMin(Node x) {
         if (x == null) return null;
         if (x.left == null) return x.right;
         x.left = deleteMin(x.left);
@@ -222,11 +224,11 @@ public class BST<Key extends Comparable<Key>, Value> {
         return x;
     }
 
-    public void delete(Key key){
+    public void delete(Key key) {
         root = delete(root, key);
     }
 
-    private Node delete(Node x, Key key){
+    private Node delete(Node x, Key key) {
         if (x == null) return null;
         int cmp = key.compareTo(x.key);
         if (cmp < 0) {
@@ -244,4 +246,8 @@ public class BST<Key extends Comparable<Key>, Value> {
         x.N = size(x.left) + size(x.right) + 1;
         return x;
     }
+
 }
+
+
+
---
+++ src/yang/al/ch03/RedBlackBST.java
@@ -0,0 +1,163 @@
+package yang.al.ch03;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by root on 15-6-2.
+ */
+public class RedBlackBST<Key extends Comparable<Key>, Value> {
+
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private Node root;
+
+    private class Node {
+        Key key;
+        Value val;
+        Node left, right;
+        int N;
+        boolean color;
+
+        public Node(Key key, Value val, int N, boolean color) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+            this.color = color;
+        }
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color;
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        return x.N;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = 1 + size(h.left)
+                + size(h.right);
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = 1 + size(h.left)
+                + size(h.right);
+        return x;
+    }
+
+    private void flipColors(Node h) {
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else return x.val;
+        }
+        return null;
+    }
+
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+        root.color = BLACK;
+    }
+
+    private Node put(Node h, Key key, Value val) {
+        if (h == null)
+            return new Node(key, val, 1, RED);
+
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) {
+            h.left = put(h.left, key, val);
+        } else if (cmp > 0) {
+            h.right = put(h.right, key, val);
+        } else {
+            h.val = val;
+        }
+
+        if (isRed(h.right) && !isRed(h.left)) {
+            h = rotateLeft(h);
+        }
+        if (isRed(h.left) && !isRed(h.left.left)) {
+            h = rotateRight(h);
+        }
+        if (isRed(h.left) && isRed(h.right)) {
+            flipColors(h);
+        }
+
+        h.N = size(h.left) + size(h.right) + 1;
+        return h;
+    }
+
+    public void deleteMin() {
+        if (isEmpty()) throw new NoSuchElementException("BST underflow");
+
+        if (!isRed(root.right) && !isRed(root.left))
+            root.color = RED;
+
+        root = deleteMin(root);
+        if (!isEmpty()) root.color = BLACK;
+    }
+
+    private Node deleteMin(Node h) {
+        if (h.left == null)
+            return null;
+
+        if (!isRed(h.left) && !isRed(h.right.left)) {
+            h = moveRedLeft(h);
+        }
+        h.left = deleteMin(h.left);
+        return balance(h);
+    }
+
+    private Node moveRedLeft(Node h) {
+        flipColors(h);
+        if (isRed(h.right.left)) {
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+
+
+
+
+}
